d50146ba444af11755e7f7d954e36ae08d98df87
mkc8 <aniketkr878876@gmail.com>
2026-05-04 18:00:41 +0530

    Add exp1.java file with KeyListener example

---
+++ exp1.java
@@ -0,0 +1,53 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyListenerEg extends Frame implements KeyListener, ActionListener {
+
+    Label l;
+    TextArea area;
+
+    KeyListenerEg() {
+        // Label creation
+        l = new Label();
+        l.setBounds(20, 50, 100, 20);
+
+        // Text area creation
+        area = new TextArea();
+        area.setBounds(20, 80, 300, 300);
+
+        area.addKeyListener(this);
+
+        add(l);
+        add(area);
+
+        setSize(400, 400);
+        setLayout(null);
+        setVisible(true);
+    }
+
+    public void keyTyped(KeyEvent var1) {
+        l.setText("Key typed");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent var1) {
+        l.setText("Key Pressed");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent var1) {
+        l.setText("Key Released");
+    }
+
+    public static void main(String args[]) {
+        new KeyListenerEg();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent var1) {
+        // No action defined
+    }
+}
