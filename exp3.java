ada7754e32e1dfc8e7f0ee38a14e1bf0ae7cc34e
mkc8 <aniketkr878876@gmail.com>
2026-05-04 18:07:42 +0530

    Create new file exp3.java with studentMarksGUIExp3 class code

---
+++ exp3.java
@@ -0,0 +1,7 @@
+public class studentMarksGUIExp3 {
+    // Class code goes here
+    public static void main(String[] args) {
+        // Example implementation
+        System.out.println("Hello, this is studentMarksGUIExp3!");
+    }
+}
